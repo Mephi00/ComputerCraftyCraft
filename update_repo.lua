697427ff4bb297b61c092f46076fc87a43377355
Mephi00 <maximhammer@icloud.com>
2025-08-21 23:14:45 +0200

    update updater

--- update_repo.lua
+++ update_repo.lua
@@ -1,2 +1,7 @@
+if not fs.exists('clone.lua') then
+  shell.run(
+    'wget https://gist.githubusercontent.com/SquidDev/e0f82765bfdefd48b0b15a5c06c0603b/raw/clone.min.lua clone.lua')
+end
+
 shell.run('rm ComputerCraftyCraft')
-shell.run('clone.min https://github.com/Mephi00/ComputerCraftyCraft')
+shell.run('clone https://github.com/Mephi00/ComputerCraftyCraft')
